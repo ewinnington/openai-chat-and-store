83d5d2cfd5749f7d42053a7fade9a52dfbb5c2ac
Eric Winnington <eric.in.the.net@gmail.com>
2023-07-09 22:09:56 +0200

    DB Postgres Storage of conversations

--- database/postgresql/initTables.sql
+++ database/postgresql/initTables.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS chatstore AUTHORIZATION CURRENT_USER;
 
-CREATE TABLE system_prompt (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, prompt_name TEXT NOT NULL, system_prompt TEXT NOT NULL);
-INSERT INTO  system_prompt (prompt_name, system_prompt) VALUES ('default', 'Hello, I am a chatbot. I am here to help you with your questions. What would you like to know?');
+CREATE TABLE system_prompt (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, prompt_name TEXT NOT NULL, system_prompt_text TEXT NOT NULL);
+INSERT INTO  system_prompt (prompt_name, system_prompt_text) VALUES ('default', 'Hello, I am a chatbot. I am here to help you with your questions. What would you like to know?');
 CREATE TABLE chat_user (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name TEXT NOT NULL, default_prompt_id INTEGER REFERENCES system_prompt(id) DEFAULT 1, input_tokens_total BIGINT NOT NULL, output_tokens_total BIGINT NOT NULL);
 CREATE TABLE conversation (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, chatuser_id INTEGER REFERENCES chat_user(id), title TEXT NOT NULL, created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,  last_active_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP );
-CREATE TABLE prompt_response (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, conversation_id INTEGER REFERENCES conversation(id), order_num INTEGER NOT NULL,  prompt JSONB NOT NULL,  response JSONB NOT NULL);
+CREATE TABLE prompt_response (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, conversation_id INTEGER REFERENCES conversation(id), order_num INTEGER NOT NULL,  prompt JSONB NOT NULL,  response JSONB);
--- src/c-commandline-dnet/Program.cs
+++ src/c-commandline-dnet/Program.cs
@@ -3,6 +3,10 @@ using Azure.AI.OpenAI;
 using static System.Environment;
 using l_dnet_embedlib;
 using System.Diagnostics;
+using System.Data.Common;
+using Npgsql;
+using Dapper;
+using System.Text.Json;
 
 class Program
 {
@@ -22,13 +26,50 @@ class Program
                 new AzureKeyCredential(key))
             : new OpenAIClient(key);
 
+        using (DbConnection connection = GetDbConnection())
+        {
+            connection.Open();
+            //Console.WriteLine("Database Version: " + connection.ServerVersion);
+            Console.WriteLine("Database State: " + connection.State);
 
+            IConversationStore store = new DBPostgresConversationStore(connection);
+            ChatUser user = store.CreateOrAquireChatUser("testuser");
+
+            DirectChat(engine, client, store, user);
+
+        }
+
+        /*
         DirectChat(engine, client);
         Console.WriteLine("  -------  ");
         CheckEmbeds(embeddings, client);
         Console.WriteLine("  -------  ");
         await StreamingChat(engine, client);
+        */
     }
+
+    private static DbConnection GetDbConnection()
+    {
+        string dbSelected = (GetEnvironmentVariable("DATABASE_TYPE") ?? "postgres").ToUpperInvariant();
+        DbConnection connection = null;
+        switch (dbSelected)
+        {
+            case "POSTGRES":
+                connection = new NpgsqlConnection(GetEnvironmentVariable("DATABASE_CONNECTION_STRING"));
+                break;
+            case "SQLITE":
+                //connection = new SqliteConnection("Data Source=:memory:");
+                //SqlMapper.AddTypeHandler(new FloatArrayTypeHandler());
+                break;
+            case "MSSQL":
+                //connection = new SqlConnection("Data Source=your-server;Initial Catalog=your-database;User ID=your-username;Password=your-password;");
+                break;
+            default:
+                throw new Exception("Database type not supported");
+        }
+        return connection;
+    }
+
 
     private static void CheckEmbeds(string embeddings, OpenAIClient client)
     {
@@ -43,7 +84,7 @@ class Program
             result2.Value.Data[0].Embedding.ToArray());
 
         float Target_Value_From_Python_check_embeds_py = 0.80373316598824f;
-        float precision                                = 0.000001f;
+        float precision = 0.000001f;
         bool isWithinTolerance = Math.Abs(Similarity - Target_Value_From_Python_check_embeds_py) < precision;
 
         Console.WriteLine("Similarity :" + Similarity + " absolute diff " + Math.Abs(Similarity - Target_Value_From_Python_check_embeds_py) + " is Within Tolerance " + isWithinTolerance);
@@ -65,15 +106,19 @@ class Program
         };
     }
 
-    private static void DirectChat(string engine, OpenAIClient client)
+    private static void DirectChat(string engine, OpenAIClient client, IConversationStore conversationStore, ChatUser user, Conversation conversation = null)
     {
         var chatCompletionsOptions = getDemoChat();
+
+        conversation = conversationStore.CreateOrAquireConversation(conversation == null ? null : conversation.Id, user);
+        PromptResponse promptResponse = conversationStore.CreateRequest(conversation, engine, chatCompletionsOptions);
 
         Response<ChatCompletions> response = client.GetChatCompletions(
             deploymentOrModelName: engine,
             chatCompletionsOptions);
 
         Console.WriteLine(response.Value.Choices[0].Message.Content);
+        conversationStore.UpdateResponse(user, conversation, promptResponse, response.Value);
     }
 
     private static async Task StreamingChat(string engine, OpenAIClient client)
@@ -99,5 +144,121 @@ class Program
 
 
 
+public class SystemPrompt
+{
+    public int Id { get; set; }
+    public string PromptName { get; set; }
+    public string SystemPromptText { get; set; }
+}
 
+public class ChatUser
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DefaultPromptId { get; set; }
+    public long InputTokensTotal { get; set; }
+    public long OutputTokensTotal { get; set; }
+}
+
+public class Conversation
+{
+    public int Id { get; set; }
+    public int ChatUserId { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastActiveAt { get; set; }
+    public Dictionary<int, PromptResponse> PromptResponses { get; set; } = new Dictionary<int, PromptResponse>();
+}
+
+public class PromptResponse
+{
+    public int Id { get; set; }
+    public int ConversationId { get; set; }
+    public int OrderNum { get; set; }
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+}
+
+public interface IConversationStore
+{
+    ChatUser CreateOrAquireChatUser(string Name);
+    Conversation CreateOrAquireConversation(int? id, ChatUser user);
+    PromptResponse CreateRequest(Conversation conversation, string engine, ChatCompletionsOptions chatCompletionsOptions);
+    void UpdateResponse(ChatUser user, Conversation conversation, PromptResponse promptResponse, ChatCompletions response);
+}
+
+public class DBPostgresConversationStore : IConversationStore
+{
+    DbConnection connection;
+
+    public DBPostgresConversationStore(DbConnection connection)
+    {
+        this.connection = connection;
+    }
+
+    public ChatUser CreateOrAquireChatUser(string Name)
+    {
+        ChatUser user = null;
+
+        user = connection.QuerySingleOrDefault<ChatUser>("SELECT * FROM chat_user WHERE Name = @Name", new { Name });
+
+        if (user == null)
+            user = connection.QuerySingleOrDefault<ChatUser>("INSERT INTO chat_user (Name, input_tokens_total, output_tokens_total) VALUES (@Name, 0, 0) RETURNING *", new { Name });
+
+        Debug.Assert(user != null);
+        return user;
+    }
+
+    public Conversation CreateOrAquireConversation(int? id, ChatUser user)
+    {
+        Conversation conversation = null;
+
+        if (id != null)
+        {
+            conversation = connection.QuerySingle<Conversation>("SELECT * FROM conversation WHERE Id = @Id", new { id });
+            Debug.Assert(conversation != null && conversation.ChatUserId != user.Id, "Conversation does not belong to user");
+            if (conversation != null)
+            {
+                //load prompt responses
+                conversation.PromptResponses = connection.Query<PromptResponse>("SELECT * FROM PromptResponse WHERE ConversationId = @Id ORDER BY order_num", new { conversation.Id }).ToDictionary(x => x.Id);
+            }
+        }
+
+        if (conversation == null)
+            conversation = connection.QuerySingle<Conversation>("INSERT INTO conversation (chatuser_id, title, created_at, last_active_at) VALUES (@ChatUserId, @Title, @CreatedAt, @LastActiveAt) RETURNING *", new { ChatUserId = user.Id, Title = "New Conversation", CreatedAt = DateTime.UtcNow, LastActiveAt = DateTime.UtcNow });
+
+        Debug.Assert(conversation != null);
+        return conversation;
+    }
+
+    public PromptResponse CreateRequest(Conversation conversation, string engine, ChatCompletionsOptions chatCompletionsOptions)
+    {
+        PromptResponse promptResponse = new PromptResponse()
+        {
+            ConversationId = conversation.Id,
+            OrderNum = conversation.PromptResponses?.Count ?? 0,
+            Prompt = JsonSerializer.Serialize(chatCompletionsOptions)
+        };
+
+        promptResponse = connection.QuerySingle<PromptResponse>("INSERT INTO prompt_response (conversation_id, order_num, Prompt) VALUES (@ConversationId, @OrderNum, @Prompt::jsonb) RETURNING *", promptResponse);
+        conversation.PromptResponses.Add(promptResponse.Id, promptResponse);
+        return promptResponse;
+    }
+
+    public void UpdateResponse(ChatUser user, Conversation conversation, PromptResponse promptResponse, ChatCompletions response)
+    {
+        promptResponse.Response = JsonSerializer.Serialize(response);
+        connection.Execute("UPDATE prompt_response SET response = @Response::jsonb WHERE Id = @Id", promptResponse);
+
+        //update user stats from the response json "Usage": {"TotalTokens": 160, "PromptTokens": 59, "CompletionTokens": 101}
+        var usage = response.Usage;
+        user.InputTokensTotal += usage.TotalTokens;
+        user.OutputTokensTotal += usage.TotalTokens;
+        connection.Execute("UPDATE chat_user SET input_tokens_total = @InputTokensTotal, output_tokens_total = @OutputTokensTotal WHERE Id = @Id", user);
+
+        //update conversation last active
+        conversation.LastActiveAt = DateTime.UtcNow; 
+        connection.Execute("UPDATE conversation SET last_active_at = @LastActiveAt WHERE Id = @Id", new { LastActiveAt = conversation.LastActiveAt, conversation.Id });
+    }
+}
 
